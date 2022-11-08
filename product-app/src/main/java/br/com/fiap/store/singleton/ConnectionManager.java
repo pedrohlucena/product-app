867f2021af27585c846ce4cadb5f831a08e80537
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-08 08:34:30 -0300

    add ConnectiomManager class

---
+++ product-app/src/main/java/br/com/fiap/store/singleton/ConnectionManager.java
@@ -0,0 +1,36 @@
+package br.com.fiap.store.singleton;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+	private static ConnectionManager instance;
+
+	private ConnectionManager(){}
+
+	public static ConnectionManager getInstance() {
+		if (instance == null)
+			instance = new ConnectionManager();
+		return instance;
+	}
+
+	public Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "user",
+					"password");
+
+			return connection;
+		} catch (SQLException e) {
+			System.err.println("Não foi possível conectar no Banco de Dados");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("O Driver JDBC não foi encontrado!");
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
