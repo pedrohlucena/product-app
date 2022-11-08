aceb30520fc47ec37892edf7f3457f0d333300a7
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-08 08:27:29 -0300

    change date property types

--- product-app/src/main/java/br/com/fiap/store/bean/Product.java
+++ product-app/src/main/java/br/com/fiap/store/bean/Product.java
@@ -1,30 +1,51 @@
 package br.com.fiap.store.bean;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.util.Calendar;
 
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	private int code;
 	private String name;
 	private int quantity;
 	private double price;
-	private Date expirationDate;
+	private Calendar manufacturingDate;
+	private Calendar expirationDate;
 	
 	public Product() {}
 	
-	public Product(String name, int quantity, double price, Date expirationDate) {
+	public Product(int code, String name, int quantity, double price, Calendar manufacturingDate, Calendar expirationDate) {
 		super();
+		this.code = code;
 		this.name = name;
 		this.quantity = quantity;
 		this.price = price;
+		this.manufacturingDate = manufacturingDate;
 		this.expirationDate = expirationDate;
 	}
 	
-	public Date getExpirationDate() {
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public Calendar getManufacturingDate() {
+		return manufacturingDate;
+	}
+
+	public void setManufacturingDate(Calendar manufacturingDate) {
+		this.manufacturingDate = manufacturingDate;
+	}
+
+	public Calendar getExpirationDate() {
 		return expirationDate;
 	}
 
-	public void setExpirationDate(Date expirationDate) {
+	public void setExpirationDate(Calendar expirationDate) {
 		this.expirationDate = expirationDate;
 	}
 
