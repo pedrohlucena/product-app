aabc9c24b542e816dc4c1a83198052ed24779c21
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-14 12:59:37 -0300

    add Encryption class and it test file

---
+++ product-app/src/main/java/br/com/fiap/store/tests/EncryptionTest.java
@@ -0,0 +1,16 @@
+package br.com.fiap.store.tests;
+
+import br.com.fiap.store.utils.Encryption;
+
+public class EncryptionTest {
+
+	public static void main(String[] args) {
+		try {
+			String password = "banAna123!";
+			String hashedPassword = Encryption.encrypt(password);
+			System.out.println(password + " " + hashedPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
