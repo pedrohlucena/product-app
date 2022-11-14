b9d2ff01d5742bd6d086b8d45cfedc5d346bb197
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-14 13:16:33 -0300

    add User class

---
+++ product-app/src/main/java/br/com/fiap/store/bean/User.java
@@ -0,0 +1,40 @@
+package br.com.fiap.store.bean;
+
+import java.io.Serializable;
+
+import br.com.fiap.store.utils.Encryption;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String password;
+	
+	public User() { super(); }
+	
+	public User(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		try {
+			this.password = Encryption.encrypt(password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
