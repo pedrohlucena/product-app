82b3b680559615febf34fe960ff54aa8ddf74940
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-14 13:29:25 -0300

    add OracleUserDAO class

---
+++ product-app/src/main/java/br/com/fiap/store/dao/impl/OracleUserDAO.java
@@ -0,0 +1,40 @@
+package br.com.fiap.store.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.fiap.store.bean.User;
+import br.com.fiap.store.dao.UserDAO;
+import br.com.fiap.store.singleton.ConnectionManager;
+
+public class OracleUserDAO implements UserDAO {
+	private Connection connection;
+	
+	@Override
+	public boolean validateUser(User user) {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			connection = ConnectionManager.getInstance().getConnection();
+			stmt = connection.prepareStatement("SELECT * FROM T_USUARIO WHERE ds_email = ? AND ds_senha = ?");
+			stmt.setString(1, user.getEmail());
+			stmt.setString(2, user.getPassword());
+			rs = stmt.executeQuery();
+			
+			if (rs.next()) { return true; };
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				rs.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+}
