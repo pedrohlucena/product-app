c9b34cdcc24aa945a879c5e93af06cb032b48ebb
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-15 19:41:58 -0300

    update DAOFactory to retrieve OracleUserDAO instance

--- product-app/src/main/java/br/com/fiap/store/factory/DAOFactory.java
+++ product-app/src/main/java/br/com/fiap/store/factory/DAOFactory.java
@@ -2,8 +2,10 @@ package br.com.fiap.store.factory;
 
 import br.com.fiap.store.dao.CategoryDAO;
 import br.com.fiap.store.dao.ProductDAO;
+import br.com.fiap.store.dao.UserDAO;
 import br.com.fiap.store.dao.impl.OracleCategoryDAO;
 import br.com.fiap.store.dao.impl.OracleProductDAO;
+import br.com.fiap.store.dao.impl.OracleUserDAO;
 
 public class DAOFactory {
 	public static ProductDAO getProductDAO() {
@@ -11,5 +13,8 @@ public class DAOFactory {
 	}
 	public static CategoryDAO getCategoryDAO() {
 		return new OracleCategoryDAO();
+	}
+	public static UserDAO getUserDAO() {
+		return new OracleUserDAO();
 	}
 }
